c28c96b4044382cea3d36f39d425164fab129dc0
Oleg Chumin <tschumin.oleg@gmail.com>
2024-10-13 11:22:38 +0300

    Обновлены или заменены классы, удален неиспользуемый

--- src/main/java/org/example/mathclient/aspect/TracingAspect.java
+++ src/main/java/org/example/mathclient/aspect/TracingAspect.java
@@ -1,60 +1,97 @@
 package org.example.mathclient.aspect;
 
+import io.jaegertracing.internal.JaegerSpanContext;
 import io.opentracing.Scope;
 import io.opentracing.Span;
 import io.opentracing.SpanContext;
 import io.opentracing.Tracer;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Slf4j
 @Aspect
-@Component
+@Component("customTracingAspect")
 public class TracingAspect {
+
+    // ff флаг работы аспекта jaeger
+    @Value("${feature-flag.ccc-jaeger.tracing.enabled}")
+    private boolean tracingEnabled;
 
     private final Tracer tracer;
     private final HttpTracingExtractorNew httpTracingExtractor;
 
-    public TracingAspect(@Qualifier("customJaegerTracer") Tracer tracer, HttpTracingExtractorNew httpTracingExtractor) {
+    @Autowired
+    public TracingAspect(@Qualifier("customJaegerTracer") Tracer tracer, @Qualifier("httpTracingExtractor") HttpTracingExtractorNew httpTracingExtractor) {
         this.tracer = tracer;
         this.httpTracingExtractor = httpTracingExtractor;
     }
 
     @Around("@within(org.springframework.web.bind.annotation.RestController)")
     public Object traceMethod(ProceedingJoinPoint pjp) throws Throwable {
+        log.debug("TracingEnabled {}", tracingEnabled);
+        if (!tracingEnabled) {
+            return pjp.proceed();
+        }
+
         String methodName = pjp.getSignature().getName();
+        log.debug("methodName: {}", methodName);
 
-        // Извлечение HttpServletRequest
-        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
-        HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+//        // Извлечение HttpServletRequest
+//        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        SpanContext spanContext = null; // Инициализация SpanContext
 
-        // Извлечение SpanContext из заголовков HTTP запроса
-        SpanContext spanContext = httpTracingExtractor.extract(request);
+//        if (requestAttributes instanceof ServletRequestAttributes) {
+//            HttpServletRequest request = (HttpServletRequest) ((ServletRequestAttributes) requestAttributes).getRequest();
+//
+//            // Извлечение SpanContext из заголовков HTTP запроса
+//            spanContext = httpTracingExtractor.extract(request);
+//        } else {
+//            log.warn("Cannot retrieve HttpServletRequest");
+//            return pjp.proceed();  // Продолжаем выполнение метода, если HttpServletRequest недоступен
+//        }
 
         // Создаем новый спан, используя извлеченный контекст (если он есть)
         Span span;
         if (spanContext != null) {
             span = tracer.buildSpan(methodName).asChildOf(spanContext).start();
-            log.info("Started tracing method: {} with extracted Trace ID: {}", methodName, span.context().toTraceId());
+            log.debug("Span ID: {}", spanContext.toSpanId());
+            log.debug("Trace ID: {}", spanContext.toTraceId());
+            log.debug("Started tracing method: {} with extracted Trace ID: {}", methodName, span.context().toTraceId());
+
+            // Проверяем, если есть родительский Span ID (зависит от реализации SpanContext)
+            if (spanContext instanceof JaegerSpanContext) {
+                JaegerSpanContext jaegerSpanContext = (JaegerSpanContext) spanContext;
+                log.info("Parent Span ID: {}", jaegerSpanContext.getParentId());
+            }
+
+            // Извлекаем baggage items (если есть)
+            spanContext.baggageItems().forEach(entry -> {
+                log.debug("Baggage item: {} -> {}", entry.getKey(), entry.getValue());
+            });
         } else {
             span = tracer.buildSpan(methodName).start();
-            log.info("Started tracing method: {} without extracted context", methodName);
+            log.debug("Started tracing method: {} without extracted context", methodName);
         }
+
+        // Логируем начало выполнения в спан
+        span.log("Starting method execution");
 
         // Активируем спан с помощью Scope
         try (Scope scope = tracer.scopeManager().activate(span)) {
             // Логируем активный спан
             Span activeSpan = tracer.activeSpan();
             if (activeSpan != null) {
-                log.info("Active span: {}", activeSpan.context().toTraceId());
+                log.debug("Active span: {}", activeSpan.context().toTraceId());
             } else {
                 log.warn("No active span found!");
             }
@@ -63,12 +100,17 @@ public class TracingAspect {
             return pjp.proceed();
         } catch (Throwable throwable) {
             span.setTag("error", true);
+            Map<String, Object> logMap = new HashMap<>();
+            logMap.put("event", "error");
+            logMap.put("error.object", throwable);
+            span.log(logMap);
             throw throwable;
         } finally {
+            // Логируем завершение в спан
+            span.log("Method execution finished");
             // Завершаем спан после выполнения метода
             span.finish();
-            log.info("Finished tracing method: {} with Trace ID: {}", methodName, span.context().toTraceId());
+            log.debug("Finished tracing method: {} with Trace ID: {}", methodName, span.context().toTraceId());
         }
     }
 }
-
